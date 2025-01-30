0540219d90e9c62ec31eeb28e551af085675b1b2
Elise Frazier <efrazie7@uncc.edu>
2025-01-29 20:16:44 -0500

    pushing mongodb connection and api setup

--- server/src/main/java/com/example/server/SocialApplication.java
+++ server/src/main/java/com/example/server/SocialApplication.java
@@ -1,13 +1,47 @@
 package com.example.server;
-
+import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+// import io.github.cdimascio.dotenv.Dotenv;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 
 @SpringBootApplication
+
 public class SocialApplication {
+	@Autowired
+	private static Environment environment;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialApplication.class, args);
+         String connectionString = environment.getProperty("spring.data.mongodb.uri");
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("admin");
+                database.runCommand(new Document("ping", 1));
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
 	}
 
 }
